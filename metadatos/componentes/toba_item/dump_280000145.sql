8b9f22b7ec9ae301dde9637f59e3f8c883829594
sapazos <sapazos@gmail.com>
2017-12-22 11:55:41 -0300

    muestro consulta en menu

--- metadatos/componentes/toba_item/dump_280000145.sql
+++ metadatos/componentes/toba_item/dump_280000145.sql
@@ -27,7 +27,7 @@ INSERT INTO apex_item (item_id, proyecto, item, padre_id, padre_proyecto, padre,
 	NULL, --descripcion
 	'280000002', --punto_montaje
 	NULL, --actividad_accion
-	'0', --menu
+	'1', --menu
 	'6', --orden
 	'0', --solicitud_registrar
 	NULL, --solicitud_obs_tipo_proyecto
