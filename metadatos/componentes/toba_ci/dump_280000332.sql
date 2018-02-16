4395b66aa2f7a1d6e55a520357eba52a660d7e30
sapazos <sapazos@gmail.com>
2018-02-16 14:57:05 -0300

    agrego reporte docentes activos sin designacion

--- metadatos/componentes/toba_ci/dump_280000332.sql
+++ metadatos/componentes/toba_ci/dump_280000332.sql
@@ -110,6 +110,18 @@ INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objet
 );
 INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objeto_proveedor, identificador, parametros_a, parametros_b, parametros_c, inicializar, orden) VALUES (
 	'planta', --proyecto
+	'280000502', --dep_id
+	'280000332', --objeto_consumidor
+	'280000583', --objeto_proveedor
+	'cuadro_sin_act', --identificador
+	NULL, --parametros_a
+	NULL, --parametros_b
+	NULL, --parametros_c
+	NULL, --inicializar
+	NULL  --orden
+);
+INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objeto_proveedor, identificador, parametros_a, parametros_b, parametros_c, inicializar, orden) VALUES (
+	'planta', --proyecto
 	'280000278', --dep_id
 	'280000332', --objeto_consumidor
 	'280000333', --objeto_proveedor
@@ -162,20 +174,27 @@ INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id)
 	'planta', --proyecto
 	'280000109', --pantalla
 	'280000332', --objeto_ci
-	'1', --orden
+	'2', --orden
 	'280000279'  --dep_id
 );
 INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id) VALUES (
 	'planta', --proyecto
 	'280000109', --pantalla
 	'280000332', --objeto_ci
-	'3', --orden
+	'4', --orden
 	'280000342'  --dep_id
 );
 INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id) VALUES (
 	'planta', --proyecto
 	'280000109', --pantalla
 	'280000332', --objeto_ci
-	'2', --orden
+	'3', --orden
 	'280000343'  --dep_id
+);
+INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id) VALUES (
+	'planta', --proyecto
+	'280000109', --pantalla
+	'280000332', --objeto_ci
+	'1', --orden
+	'280000502'  --dep_id
 );
--- php/consultas/co_inicio.php
+++ php/consultas/co_inicio.php
@@ -14,45 +14,57 @@ class co_inicio
         ";
 	return toba::db()->consultar($sql);
     }
+    
+    function get_activos_sin_designacion()
+    {
+        $sql = "SELECT  DISTINCT
+                        apellido || ', ' || nombres as nombre_completo
+                FROM    personas 
+                WHERE   persona not in (SELECT des2.persona FROM designaciones as des2 WHERE des2.estado in (1,4,5) and des2.persona = persona)
+                        AND personas.estado_docente = 1
+        ORDER BY nombre_completo
+        ";
+	return toba::db()->consultar($sql);
+    }
 
     function get_vencimientos($desde, $hasta)
     {
 	$sql = "
-SELECT asignacion,
-	'Asignacion' as tipo,
-	personas.apellido || ', ' || personas.nombres as nombre_completo,
-	actividades.descripcion as actividad,
-	asignaciones.fecha_desde,
-	asignaciones.fecha_hasta,
-	asignaciones.resolucion || '/' || asignaciones.resolucion_anio as resol_numero,
-	estados.estado,
-	estados.descripcion as estado_desc
-FROM asignaciones, personas, actividades, estados
-WHERE asignaciones.persona = personas.persona
-AND asignaciones.actividad = actividades.actividad
-AND asignaciones.estado = estados.estado
-AND fecha_hasta between '$desde'::date and '$hasta'::date
-AND asignaciones.estado in (1)
+                SELECT asignacion,
+                        'Asignacion' as tipo,
+                        personas.apellido || ', ' || personas.nombres as nombre_completo,
+                        actividades.descripcion as actividad,
+                        asignaciones.fecha_desde,
+                        asignaciones.fecha_hasta,
+                        asignaciones.resolucion || '/' || asignaciones.resolucion_anio as resol_numero,
+                        estados.estado,
+                        estados.descripcion as estado_desc
+                FROM asignaciones, personas, actividades, estados
+                WHERE asignaciones.persona = personas.persona
+                AND asignaciones.actividad = actividades.actividad
+                AND asignaciones.estado = estados.estado
+                AND fecha_hasta between '$desde'::date and '$hasta'::date
+                AND asignaciones.estado in (1)
 
-UNION 
+                UNION 
 
-SELECT designacion,
-	'Designacion' as tipo,
-	personas.apellido || ', ' || personas.nombres as nombre_completo,
-	espacios_disciplinares.descripcion as actividad,
-	designaciones.fecha_desde,
-	designaciones.fecha_hasta,
-	designaciones.resolucion || '/' || designaciones.resolucion_anio as resol_numero,
-	estados.estado,
-	estados.descripcion as estado_desc
-FROM designaciones, personas, espacios_disciplinares, estados
-WHERE designaciones.persona = personas.persona
-AND designaciones.espacio_disciplinar = espacios_disciplinares.espacio_disciplinar
-AND designaciones.estado = estados.estado
-AND fecha_hasta between '$desde'::date and '$hasta'::date
-AND designaciones.estado in (1)
+                SELECT designacion,
+                        'Designacion' as tipo,
+                        personas.apellido || ', ' || personas.nombres as nombre_completo,
+                        espacios_disciplinares.descripcion as actividad,
+                        designaciones.fecha_desde,
+                        designaciones.fecha_hasta,
+                        designaciones.resolucion || '/' || designaciones.resolucion_anio as resol_numero,
+                        estados.estado,
+                        estados.descripcion as estado_desc
+                FROM designaciones, personas, espacios_disciplinares, estados
+                WHERE designaciones.persona = personas.persona
+                AND designaciones.espacio_disciplinar = espacios_disciplinares.espacio_disciplinar
+                AND designaciones.estado = estados.estado
+                AND fecha_hasta between '$desde'::date and '$hasta'::date
+                AND designaciones.estado in (1)
 
-ORDER BY nombre_completo
+                ORDER BY nombre_completo
 		";
 	return toba::db()->consultar($sql);
     }
@@ -60,41 +72,41 @@ ORDER BY nombre_completo
     function get_asignaciones_periodo_menor($hasta)
     {
 	$sql = "
-SELECT asignacion,
-	'Aasignacion' as tipo,
-	personas.apellido || ', ' || personas.nombres as nombre_completo,
-	actividades.descripcion as actividad,
-	asignaciones.fecha_desde,
-	asignaciones.fecha_hasta,
-	asignaciones.resolucion || '/' || asignaciones.resolucion_anio as resol_numero,
-	estados.estado,
-	estados.descripcion as estado_desc
-FROM asignaciones, personas, actividades, estados
-WHERE asignaciones.persona = personas.persona
-AND asignaciones.actividad = actividades.actividad
-AND asignaciones.estado = estados.estado
-AND fecha_hasta < '$hasta'
-AND asignaciones.estado in (1)
+                SELECT asignacion,
+                        'Aasignacion' as tipo,
+                        personas.apellido || ', ' || personas.nombres as nombre_completo,
+                        actividades.descripcion as actividad,
+                        asignaciones.fecha_desde,
+                        asignaciones.fecha_hasta,
+                        asignaciones.resolucion || '/' || asignaciones.resolucion_anio as resol_numero,
+                        estados.estado,
+                        estados.descripcion as estado_desc
+                FROM asignaciones, personas, actividades, estados
+                WHERE asignaciones.persona = personas.persona
+                AND asignaciones.actividad = actividades.actividad
+                AND asignaciones.estado = estados.estado
+                AND fecha_hasta < '$hasta'
+                AND asignaciones.estado in (1)
 
-UNION 
+                UNION 
 
-SELECT designacion,
-	'Designacion' as tipo,
-	personas.apellido || ', ' || personas.nombres as nombre_completo,
-	espacios_disciplinares.descripcion as actividad,
-	designaciones.fecha_desde,
-	designaciones.fecha_hasta,
-	designaciones.resolucion || '/' || designaciones.resolucion_anio as resol_numero,
-	estados.estado,
-	estados.descripcion as estado_desc
-FROM designaciones, personas, espacios_disciplinares, estados
-WHERE designaciones.persona = personas.persona
-AND designaciones.espacio_disciplinar = espacios_disciplinares.espacio_disciplinar
-AND designaciones.estado = estados.estado
-AND fecha_hasta < '$hasta'
-AND designaciones.estado in (1)
+                SELECT designacion,
+                        'Designacion' as tipo,
+                        personas.apellido || ', ' || personas.nombres as nombre_completo,
+                        espacios_disciplinares.descripcion as actividad,
+                        designaciones.fecha_desde,
+                        designaciones.fecha_hasta,
+                        designaciones.resolucion || '/' || designaciones.resolucion_anio as resol_numero,
+                        estados.estado,
+                        estados.descripcion as estado_desc
+                FROM designaciones, personas, espacios_disciplinares, estados
+                WHERE designaciones.persona = personas.persona
+                AND designaciones.espacio_disciplinar = espacios_disciplinares.espacio_disciplinar
+                AND designaciones.estado = estados.estado
+                AND fecha_hasta < '$hasta'
+                AND designaciones.estado in (1)
 
-ORDER BY nombre_completo
+                ORDER BY nombre_completo
 		";
 	return toba::db()->consultar($sql);
     }
