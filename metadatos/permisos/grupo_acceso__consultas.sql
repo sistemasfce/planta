43dcc4ec6a1ceadbf0b81cf870047dce32db178e
sapazos <sapazos@gmail.com>
2018-04-05 08:45:02 -0300

    agrego perfil funcional consultas

---
+++ metadatos/permisos/grupo_acceso__consultas.sql
@@ -0,0 +1,316 @@
+
+------------------------------------------------------------
+-- apex_usuario_grupo_acc
+------------------------------------------------------------
+INSERT INTO apex_usuario_grupo_acc (proyecto, usuario_grupo_acc, nombre, nivel_acceso, descripcion, vencimiento, dias, hora_entrada, hora_salida, listar, permite_edicion, menu_usuario) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	'consultas', --nombre
+	NULL, --nivel_acceso
+	'Usuario para consultar datos', --descripcion
+	NULL, --vencimiento
+	NULL, --dias
+	NULL, --hora_entrada
+	NULL, --hora_salida
+	NULL, --listar
+	'0', --permite_edicion
+	NULL  --menu_usuario
+);
+
+------------------------------------------------------------
+-- apex_usuario_grupo_acc_item
+------------------------------------------------------------
+
+--- INICIO Grupo de desarrollo 0
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'1'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'2'  --item
+);
+--- FIN Grupo de desarrollo 0
+
+--- INICIO Grupo de desarrollo 280
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000010'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000038'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000039'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000040'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000041'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000042'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000043'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000044'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000045'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000046'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000047'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000048'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000049'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000050'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000051'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000052'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000053'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000054'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000055'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000056'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000057'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000062'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000063'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000065'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000066'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000067'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000068'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000069'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000074'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000075'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000076'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000077'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000078'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000079'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000080'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000084'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000111'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000112'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000113'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000144'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000145'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000146'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000149'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000150'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000151'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000165'  --item
+);
+--- FIN Grupo de desarrollo 280
--- php/operaciones/consultas/autoevaluaciones/ci_autoeval_pendientes_act.php
+++ php/operaciones/consultas/autoevaluaciones/ci_autoeval_pendientes_act.php
@@ -1,41 +1,40 @@
 <?php
-class ci_autoeval_pendientes_act extends planta_ci
-{
-	protected $s__filtro;
-	
-	//-----------------------------------------------------------------------------------
-	//---- cuadro -----------------------------------------------------------------------
-	//-----------------------------------------------------------------------------------
 
-	function conf__cuadro(planta_ei_cuadro $cuadro)
-	{
-		$where = $this->dep('filtro')->get_sql_where();
-		if ($where == '1=1')
-			return;
-		$datos = toba::consulta_php('co_autoevaluaciones')->get_act_pendientes($where);
-		$cuadro->set_titulo('Docentes que no completaron o no confirmaron su autoevaluación');
-		$cuadro->set_datos($datos);        
-	}        
-	
-	//-----------------------------------------------------------------------------------
-	//---- filtro -----------------------------------------------------------------------
-	//-----------------------------------------------------------------------------------
+class ci_autoeval_pendientes_act extends planta_ci {
 
-	function conf__filtro(planta_ei_filtro $filtro)
-	{
-		if (isset($this->s__filtro)) {
-			$filtro->set_datos($this->s__filtro);
-		}
-	}
+    protected $s__filtro;
 
-	function evt__filtro__filtrar($datos)
-	{
-		$this->s__filtro = $datos;
-	}
+    //-----------------------------------------------------------------------------------
+    //---- cuadro -----------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------
 
-	function evt__filtro__cancelar()
-	{
-		unset($this->s__filtro);
-	}    
+    function conf__cuadro(planta_ei_cuadro $cuadro) {
+        $where = $this->dep('filtro')->get_sql_where();
+        if ($where == '1=1')
+            return;
+        $datos = toba::consulta_php('co_autoevaluaciones')->get_act_pendientes($where);
+        $cuadro->set_titulo('Docentes que no completaron o no confirmaron su autoevaluacion');
+        $cuadro->set_datos($datos);
+    }
+
+    //-----------------------------------------------------------------------------------
+    //---- filtro -----------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------
+
+    function conf__filtro(planta_ei_filtro $filtro) {
+        if (isset($this->s__filtro)) {
+            $filtro->set_datos($this->s__filtro);
+        }
+    }
+
+    function evt__filtro__filtrar($datos) {
+        $this->s__filtro = $datos;
+    }
+
+    function evt__filtro__cancelar() {
+        unset($this->s__filtro);
+    }
+
 }
+
 ?>
