4daf0c0a0e5581d00e1ddca44ec16b75bef98348
sapazos <sapazos@gmail.com>
2018-05-10 14:36:06 -0300

    agrego campo responsable a consulta asignaciones

--- metadatos/componentes/toba_ei_filtro/dump_280000187.sql
+++ metadatos/componentes/toba_ei_filtro/dump_280000187.sql
@@ -429,7 +429,7 @@ INSERT INTO apex_objeto_ei_filtro_col (objeto_ei_filtro_col, objeto_ei_filtro, o
 	NULL, --descripcion
 	'0', --obligatorio
 	'1', --inicial
-	'8', --orden
+	'9', --orden
 	NULL, --estado_defecto
 	'1', --opciones_es_multiple
 	'ef_multi_seleccion_check', --opciones_ef
@@ -603,7 +603,7 @@ INSERT INTO apex_objeto_ei_filtro_col (objeto_ei_filtro_col, objeto_ei_filtro, o
 	NULL, --descripcion
 	'0', --obligatorio
 	'1', --inicial
-	'9', --orden
+	'10', --orden
 	NULL, --estado_defecto
 	'0', --opciones_es_multiple
 	NULL, --opciones_ef
@@ -661,7 +661,7 @@ INSERT INTO apex_objeto_ei_filtro_col (objeto_ei_filtro_col, objeto_ei_filtro, o
 	NULL, --descripcion
 	'0', --obligatorio
 	'1', --inicial
-	'10', --orden
+	'11', --orden
 	NULL, --estado_defecto
 	'0', --opciones_es_multiple
 	NULL, --opciones_ef
@@ -719,7 +719,7 @@ INSERT INTO apex_objeto_ei_filtro_col (objeto_ei_filtro_col, objeto_ei_filtro, o
 	NULL, --descripcion
 	'0', --obligatorio
 	'1', --inicial
-	'11', --orden
+	'12', --orden
 	NULL, --estado_defecto
 	'0', --opciones_es_multiple
 	'ef_combo', --opciones_ef
@@ -764,6 +764,64 @@ INSERT INTO apex_objeto_ei_filtro_col (objeto_ei_filtro_col, objeto_ei_filtro, o
 	NULL, --selec_ancho
 	NULL, --selec_serializar
 	NULL, --selec_cant_columnas
+	NULL  --placeholder
+);
+INSERT INTO apex_objeto_ei_filtro_col (objeto_ei_filtro_col, objeto_ei_filtro, objeto_ei_filtro_proyecto, tipo, nombre, expresion, etiqueta, descripcion, obligatorio, inicial, orden, estado_defecto, opciones_es_multiple, opciones_ef, carga_metodo, carga_clase, carga_include, carga_dt, carga_consulta_php, carga_sql, carga_fuente, carga_lista, carga_col_clave, carga_col_desc, carga_permite_no_seteado, carga_no_seteado, carga_no_seteado_ocultar, carga_maestros, edit_tamano, edit_maximo, edit_mascara, edit_unidad, edit_rango, edit_expreg, estilo, popup_item, popup_proyecto, popup_editable, popup_ventana, popup_carga_desc_metodo, popup_carga_desc_clase, popup_carga_desc_include, popup_puede_borrar_estado, punto_montaje, check_valor_si, check_valor_no, check_desc_si, check_desc_no, selec_cant_minima, selec_cant_maxima, selec_utilidades, selec_tamano, selec_ancho, selec_serializar, selec_cant_columnas, placeholder) VALUES (
+	'280000232', --objeto_ei_filtro_col
+	'280000187', --objeto_ei_filtro
+	'planta', --objeto_ei_filtro_proyecto
+	'opciones', --tipo
+	'responsable', --nombre
+	'asignaciones.responsable', --expresion
+	'Responsable', --etiqueta
+	NULL, --descripcion
+	'0', --obligatorio
+	'1', --inicial
+	'8', --orden
+	NULL, --estado_defecto
+	'1', --opciones_es_multiple
+	'ef_multi_seleccion_check', --opciones_ef
+	NULL, --carga_metodo
+	NULL, --carga_clase
+	NULL, --carga_include
+	NULL, --carga_dt
+	NULL, --carga_consulta_php
+	NULL, --carga_sql
+	'planta', --carga_fuente
+	'S/Sí,N/No', --carga_lista
+	NULL, --carga_col_clave
+	NULL, --carga_col_desc
+	'0', --carga_permite_no_seteado
+	NULL, --carga_no_seteado
+	NULL, --carga_no_seteado_ocultar
+	NULL, --carga_maestros
+	NULL, --edit_tamano
+	NULL, --edit_maximo
+	NULL, --edit_mascara
+	NULL, --edit_unidad
+	NULL, --edit_rango
+	NULL, --edit_expreg
+	NULL, --estilo
+	NULL, --popup_item
+	NULL, --popup_proyecto
+	NULL, --popup_editable
+	NULL, --popup_ventana
+	NULL, --popup_carga_desc_metodo
+	NULL, --popup_carga_desc_clase
+	NULL, --popup_carga_desc_include
+	NULL, --popup_puede_borrar_estado
+	'280000002', --punto_montaje
+	NULL, --check_valor_si
+	NULL, --check_valor_no
+	NULL, --check_desc_si
+	NULL, --check_desc_no
+	NULL, --selec_cant_minima
+	NULL, --selec_cant_maxima
+	'0', --selec_utilidades
+	NULL, --selec_tamano
+	NULL, --selec_ancho
+	'0', --selec_serializar
+	'2', --selec_cant_columnas
 	NULL  --placeholder
 );
 --- FIN Grupo de desarrollo 280
