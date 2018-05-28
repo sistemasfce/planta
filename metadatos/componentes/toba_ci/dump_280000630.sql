7731d2db539cb39df1b1eb62918670dfffcca5d0
sapazos <sapazos@gmail.com>
2018-05-28 13:14:23 -0300

    agrego formulario con descripcion a las consultas de acreeditacion

--- metadatos/componentes/toba_ci/dump_280000630.sql
+++ metadatos/componentes/toba_ci/dump_280000630.sql
@@ -84,6 +84,18 @@ INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objet
 	NULL, --inicializar
 	NULL  --orden
 );
+INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objeto_proveedor, identificador, parametros_a, parametros_b, parametros_c, inicializar, orden) VALUES (
+	'planta', --proyecto
+	'280000544', --dep_id
+	'280000630', --objeto_consumidor
+	'280000633', --objeto_proveedor
+	'form', --identificador
+	NULL, --parametros_a
+	NULL, --parametros_b
+	NULL, --parametros_c
+	NULL, --inicializar
+	NULL  --orden
+);
 --- FIN Grupo de desarrollo 280
 
 ------------------------------------------------------------
@@ -96,11 +108,11 @@ INSERT INTO apex_objeto_ci_pantalla (objeto_ci_proyecto, objeto_ci, pantalla, id
 	'280000630', --objeto_ci
 	'280000193', --pantalla
 	'pant_inicial', --identificador
-	NULL, --orden
+	'1', --orden
 	'Pantalla Inicial', --etiqueta
 	NULL, --descripcion
 	NULL, --tip
-	NULL, --imagen_recurso_origen
+	'apex', --imagen_recurso_origen
 	NULL, --imagen
 	NULL, --objetos
 	NULL, --eventos
@@ -108,7 +120,7 @@ INSERT INTO apex_objeto_ci_pantalla (objeto_ci_proyecto, objeto_ci, pantalla, id
 	NULL, --subclase_archivo
 	NULL, --template
 	NULL, --template_impresion
-	NULL  --punto_montaje
+	'280000002'  --punto_montaje
 );
 --- FIN Grupo de desarrollo 280
 
@@ -119,6 +131,13 @@ INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id)
 	'planta', --proyecto
 	'280000193', --pantalla
 	'280000630', --objeto_ci
-	'0', --orden
+	'1', --orden
 	'280000542'  --dep_id
+);
+INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id) VALUES (
+	'planta', --proyecto
+	'280000193', --pantalla
+	'280000630', --objeto_ci
+	'0', --orden
+	'280000544'  --dep_id
 );
