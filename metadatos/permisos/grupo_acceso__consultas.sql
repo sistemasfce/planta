dbf72aea9cfe3d683caa933d4f0d0df4ea523863
sapazos <sapazos@gmail.com>
2019-03-27 09:31:20 -0300

    agrego permisos para los perfiles consultas

--- metadatos/permisos/grupo_acceso__consultas.sql
+++ metadatos/permisos/grupo_acceso__consultas.sql
@@ -319,4 +319,10 @@ INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, i
 	NULL, --item_id
 	'280000226'  --item
 );
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000228'  --item
+);
 --- FIN Grupo de desarrollo 280
--- metadatos/permisos/grupo_acceso__usuario.sql
+++ metadatos/permisos/grupo_acceso__usuario.sql
@@ -607,4 +607,10 @@ INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, i
 	NULL, --item_id
 	'280000225'  --item
 );
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'usuario', --usuario_grupo_acc
+	NULL, --item_id
+	'280000228'  --item
+);
 --- FIN Grupo de desarrollo 280
