af371f5470190025da4933b9a50d98a07507c08b
sapazos <sapazos@gmail.com>
2019-05-06 09:24:27 -0300

    texto corregido en extension periodo

--- metadatos/componentes/toba_ei_formulario/dump_280000254.sql
+++ metadatos/componentes/toba_ei_formulario/dump_280000254.sql
@@ -376,7 +376,7 @@ INSERT INTO apex_objeto_ei_formulario_ef (objeto_ei_formulario_fila, objeto_ei_f
 	NULL, --carga_consulta_php
 	NULL, --carga_sql
 	'planta', --carga_fuente
-	'PAR_AUTOEVAL_VENCE/Autoevaluación,PAR_EVAL_VENCE', --carga_lista
+	'PAR_AUTOEVAL_VENCE/Autoevaluación,PAR_EVAL_VENCE/Evaluación', --carga_lista
 	NULL, --carga_col_clave
 	NULL, --carga_col_desc
 	NULL, --carga_maestros
