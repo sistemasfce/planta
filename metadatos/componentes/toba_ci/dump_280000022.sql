20f89c2218b3e09b4958e4cf53ae418f1b29e3d1
sapazos <sapazos@gmail.com>
2020-07-31 13:17:45 -0300

    saco eliminar persona

--- metadatos/componentes/toba_ci/dump_280000022.sql
+++ metadatos/componentes/toba_ci/dump_280000022.sql
@@ -124,7 +124,7 @@ INSERT INTO apex_objeto_eventos (proyecto, evento_id, objeto, identificador, eti
 	NULL, --estilo
 	'apex', --imagen_recurso_origen
 	'borrar.png', --imagen
-	'1', --en_botonera
+	'0', --en_botonera
 	NULL, --ayuda
 	'3', --orden
 	NULL, --ci_predep
