fba1f9eac50c5865bd0368adfb406abc6bfe1605
sapazos <sapazos@gmail.com>
2020-08-01 10:30:06 -0300

    agrego cambio password

--- metadatos/permisos/grupo_acceso__admin.sql
+++ metadatos/permisos/grupo_acceso__admin.sql
@@ -751,4 +751,10 @@ INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, i
 	NULL, --item_id
 	'280000278'  --item
 );
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'admin', --usuario_grupo_acc
+	NULL, --item_id
+	'280000290'  --item
+);
 --- FIN Grupo de desarrollo 280
