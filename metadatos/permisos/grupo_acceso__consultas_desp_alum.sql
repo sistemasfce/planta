a2dc19af0dd663275e7c982c4375e05d0a6c6c67
sapazos <sapazos@gmail.com>
2022-04-08 09:26:20 -0300

    los metadatos del perfil despacho-alumnos

---
+++ metadatos/permisos/grupo_acceso__consultas_desp_alum.sql
@@ -0,0 +1,154 @@
+
+------------------------------------------------------------
+-- apex_usuario_grupo_acc
+------------------------------------------------------------
+INSERT INTO apex_usuario_grupo_acc (proyecto, usuario_grupo_acc, nombre, nivel_acceso, descripcion, vencimiento, dias, hora_entrada, hora_salida, listar, permite_edicion, menu_usuario) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	'consultas_desp_alum', --nombre
+	NULL, --nivel_acceso
+	'Consultas asignaciones y personas despacho - alumn', --descripcion
+	NULL, --vencimiento
+	NULL, --dias
+	NULL, --hora_entrada
+	NULL, --hora_salida
+	NULL, --listar
+	'0', --permite_edicion
+	NULL  --menu_usuario
+);
+
+------------------------------------------------------------
+-- apex_usuario_grupo_acc_item
+------------------------------------------------------------
+
+--- INICIO Grupo de desarrollo 0
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'1'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'2'  --item
+);
+--- FIN Grupo de desarrollo 0
+
+--- INICIO Grupo de desarrollo 280
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'280000010'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'280000041'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'280000045'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'280000046'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'280000047'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'280000068'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'280000069'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'280000074'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'280000075'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'280000076'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'280000084'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'280000106'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'280000111'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'280000146'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'280000150'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'280000151'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'280000165'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'280000166'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'planta', --proyecto
+	'consultas_desp_alum', --usuario_grupo_acc
+	NULL, --item_id
+	'280000183'  --item
+);
+--- FIN Grupo de desarrollo 280
